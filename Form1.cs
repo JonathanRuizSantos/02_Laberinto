4438f7e36940eccb9c9548f6e19e02938066b5f3
Jonathan Ruiz Santos <jruizs1800@alumno.ipn.mx>
2022-09-09 16:59:29 -0500

    V2

--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,15 @@ namespace WinFormsApp1
         int ncol = 11;
         int x = 0;
         int y = 0;
+        int cx = 0;
+        int cy = 0;
         int[] Arreglo = new int[2];
-        int[,] Laberinto = new int[11,11];
+        int[,] Laberinto = new int[11, 11];
         int[] Coordenadas = new int[2];
+        int[] Coor = new int[2];
         Queue<int[]> cola = new Queue<int[]>();
+        
+
 
         public Form1()
         {
@@ -89,73 +94,80 @@ namespace WinFormsApp1
 
         private void btSolucion_Click(object sender, EventArgs e)
         {
-            
+            lbCola.Items.Clear();
+            lbPila.Items.Clear();
             Arreglo[0] = 0;
             Arreglo[1] = 0;
             cola.Enqueue(Arreglo);
-            while(cola.Count() > 0) 
+            imprime_Cola();
+
+            int contador_cola = cola.Count;
+            while (contador_cola>0)
             {
+                
                 Coordenadas = cola.Dequeue();
                 x = Coordenadas[0];
                 y = Coordenadas[1];
+                lbPila.Items.Add(x + "," + y + " Es la coordenada elimnada");
 
-                for(int Edo = 0; Edo < 4; Edo++)
+                for (int Edo=0; Edo<4; Edo++)
                 {
-                    if (Edo == 0 && x != 0 && Laberinto[x - 1, y] == 0)
+                    if (Edo==0 && x!=0 && Laberinto[x-1,y]==0)
                     {
-                        x = Coordenadas[x - 1];
-                        y = Coordenadas[y];
+                        //Coor[0] = x - 1;
+                        //Coor[1] = y;
+                        //guardado_Cola();
+                    }
+                    else if (Edo==1 && x!=10 && Laberinto[x+1,y]==0)
+                    {
+                        Coor[0] = x + 1;
+                        Coor[1] = y;
                         guardado_Cola();
                     }
-                    else if (Edo == 1 && x != 10 && Laberinto[x + 1, y] == 0)
+                    else if (Edo==2 && y!=0 && Laberinto[x,y-1] == 0)
                     {
-                        x = Coordenadas[x + 1];
-                        y = Coordenadas[y];
-                        guardado_Cola();
+                        //Coor[0] = x;
+                        //Coor[1] = y-1;
+                        //guardado_Cola(); 
                     }
-                    else if (Edo == 2 && y != 0 && Laberinto[x, y - 1] == 0)
+                    else if (Edo==3 && y!=10 && Laberinto[x,y+1]==0)
                     {
-                        x = Coordenadas[x];
-                        y = Coordenadas[y - 1];
-                        guardado_Cola();
-                    }
-                    else if (Edo == 3 && y != 10 && Laberinto[x, y + 1] == 0)
-                    {
-                        x = Coordenadas[x];
-                        y = Coordenadas[y + 1];
+                        Coor[0] = x;
+                        Coor[1] = y+1;
                         guardado_Cola();
                     }
                 }
-
                 imprime_Cola();
-                
+                contador_cola = cola.Count;
             }
-            
-            
         }
-
         public void imprime_Cola()
         {
             string suma = "";
-            foreach (int[] i in cola)
+            int contador = 0;
+            
+            while (contador <= cola.Count)
             {
-                suma += i.ToString();
+                foreach (int[] ind in cola)
+                {
+                    suma = suma + ind[contador];
+                }
+                contador++;
             }
+
             lbCola.Items.Add(". " + suma);
         }
         public void guardado_Cola()
         {
-            if (x == 10 && y == 10)
+            if (cola.Contains(Coor))
             {
-                MessageBox.Show("El laberinto tiene solucion");
+                lbPila.Items.Add(Coor[0] + "," + Coor[1] + " ya esta en la cola");
             }
             else
             {
-                Arreglo[0] = x;
-                Arreglo[1] = y;
-                cola.Enqueue(Arreglo);
+                cola.Enqueue(Coor);
+                lbPila.Items.Add(Coor[0] + "," + Coor[1] + " Se agrego a cola");
             }
-        }
-        
+        }  
     }
 }
